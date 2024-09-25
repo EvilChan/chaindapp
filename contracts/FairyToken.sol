f4fcb1ffdebc20763ebaf65e4a9506e98c86746c
EvilChan <1395822743@qq.com>
2024-09-25 16:55:47 +0800

    FairyToken只有合约部署者可以mint代币

--- contracts/FairyToken.sol
+++ contracts/FairyToken.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FairyToken is ERC20 {
-    constructor() ERC20("Fairy", "FAY") {}
 
-    function mint(uint256 supply) public {
+contract FairyToken is Ownable, ERC20 {
+    constructor() Ownable(msg.sender) ERC20("Fairy", "FAY") {}
+
+    function mint(uint256 supply) public onlyOwner {
         _mint(msg.sender, supply);
     }
 }
